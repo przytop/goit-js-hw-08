464eebf6c31c12c90d6dd487054c23758fa7b701
Paweł <102540215+przytop@users.noreply.github.com>
2024-07-13 18:13:44 +0200

    prepared for task

--- js/task-1.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-const items = document.querySelectorAll("#categories .item");
-
-console.log("Number of categories:", items.length);
-items.forEach((item) => {
-  console.log("Category:", item.querySelector("h2").textContent);
-  console.log("Elements:", item.querySelectorAll("li").length);
-});
